3cb793aa10cbd3eae11936cfe91e34927dbe464a
WTC <zngeyane023@student.wethinkcode.co.za>
2024-11-04 16:11:01 +0200

    constructor for Task

--- ToDoList/Task.cs
+++ ToDoList/Task.cs
@@ -5,4 +5,9 @@ public class Task{
 public int ID {get; set;}
 public string Decsription {get; set;}
 public bool IsComplete {get; set;}
+
+public Task (int id, string description){
+ID = id;
+Description = description;
+IsComplete = false;}
 }
